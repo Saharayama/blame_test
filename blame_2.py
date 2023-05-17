98cdd6cf7b2faf0a46fe70838fe59510df98f96d
Saharayama <47473618+Saharayama@users.noreply.github.com>
2023-05-17 20:52:50 +0900

    update

--- blame_2.py
+++ blame_2.py
@@ -28,13 +28,14 @@ with open(list_file, 'r', encoding='utf-8') as f_list:
 with open(src_file, 'r', encoding='utf-8') as f_src:
     for l_src in f_src:
         l_src = l_src.rstrip()
-        if re.findall(l_all_list, l_src):
-            write_out()
-        else:
-            num_src = l_src.split(',')[1]
-            file_src = l_src.split(',')[2]
-            cmd = 'git -C ' + repo_dir + ' blame -wL ' + num_src + ',' + num_src + ' ' + file_src
-            res = subprocess.run(cmd, stdout=subprocess.PIPE)
-            if str(res.stdout)[2] != '^':
+        if l_src != '':
+            if re.findall(l_all_list, l_src):
                 write_out()
-                print('blame', l_src)
+            else:
+                num_src = l_src.split(',')[1]
+                file_src = l_src.split(',')[2]
+                cmd = 'git -C ' + repo_dir + ' blame -wL ' + num_src + ',' + num_src + ' ' + file_src
+                res = subprocess.run(cmd, stdout=subprocess.PIPE)
+                if str(res.stdout)[2] != '^':
+                    write_out()
+                    print('blame', l_src)
